2c0ad2203c341a7340f9f61690046a74d99a82c1
kavinrajan <kavinece53@gmail.com>
2026-02-23 23:13:24 +0530

    ChecklyInn project | Gradle Changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
 rootProject.name = "ChecklyInn-Server"
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+/*plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}*/
